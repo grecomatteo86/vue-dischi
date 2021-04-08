e00f29a1f80d2fc26b8970ad9102fe2452d73bd3
Matteo <grecomatteo86@gmail.com>
2021-04-08 15:41:55 +0200

    cd container emptying and setting 2 static album's obj

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-
+var app = new Vue (
+  {
+    el:"#app",
+    data:{
+      albums:[
+        {
+          poster: "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+          title: "New Jersey",
+          author: "Bon Jovi",
+          genre: "Rock",
+          year: "1988"
+        },
+        {
+          poster: "https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg",
+          title: "Live at Wembley 86",
+          author: "Queen",
+          genre: "Pop",
+          year: "1992"
+        }
+      ]
+    }
+  }
+)

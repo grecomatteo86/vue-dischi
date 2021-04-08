2c0020902604b4a0ced27abb66c635e2ef14fcec
Matteo <grecomatteo86@gmail.com>
2021-04-08 22:09:49 +0200

    sorting by year

--- js/script.js
+++ js/script.js
@@ -9,18 +9,17 @@ var app = new Vue (
     mounted:function(){
       axios.get('https://flynn.boolean.careers/exercises/api/array/music')
       .then((response) => {
+        // ASSEGN0 i dati server al mio NUOVO array albums
         this.albums = response.data.response;
-
-        
-
+        //con il forEach mi prendo i genre dall'array albums e li pusho nel NUOVO array genres
         this.albums.forEach((item) => {
           // console.log(item.genre);
           if (!this.genres.includes(item.genre)) {
             this.genres.push(item.genre);
-
           }
-
         });
+        //ordino gli albums secondo lo year
+        this.albums.sort((a,b) => (a.year > b.year) ? 1 : -1);
 
         console.log(this.genres);
       });

d3f05de515d4c95956b407e205d02067818e7a85
Matteo <grecomatteo86@gmail.com>
2021-04-08 16:26:20 +0200

    axios setting and css mods

--- js/script.js
+++ js/script.js
@@ -3,21 +3,15 @@ var app = new Vue (
     el:"#app",
     data:{
       albums:[
-        {
-          poster: "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
-          title: "New Jersey",
-          author: "Bon Jovi",
-          genre: "Rock",
-          year: "1988"
-        },
-        {
-          poster: "https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg",
-          title: "Live at Wembley 86",
-          author: "Queen",
-          genre: "Pop",
-          year: "1992"
-        }
+
       ]
+    },
+    mounted:function(){
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((response) => {
+        // console.log(response.data.response);
+        this.albums = response.data.response;
+      });
     }
   }
 )

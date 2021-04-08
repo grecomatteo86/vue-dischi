ccc3f70bf039086a6752f5cdeacd85d66a99f11e
Matteo <grecomatteo86@gmail.com>
2021-04-08 18:04:49 +0200

    dynamic select

--- js/script.js
+++ js/script.js
@@ -2,15 +2,22 @@ var app = new Vue (
   {
     el:"#app",
     data:{
-      albums:[
-
-      ]
+      albums:[],
+      selected:'',
+      genres:[],
     },
     mounted:function(){
       axios.get('https://flynn.boolean.careers/exercises/api/array/music')
       .then((response) => {
-        // console.log(response.data.response);
         this.albums = response.data.response;
+
+        this.albums.forEach((item) => {
+          // console.log(item.genre);
+          if (!this.genres.includes(item.genre)) {
+            this.genres.push(item.genre);
+          }
+        });
+        console.log(this.genres);
       });
     }
   }
